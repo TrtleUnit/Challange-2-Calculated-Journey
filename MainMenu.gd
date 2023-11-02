0463703a9308e1b5fa09f58c0dd45b95ea05dfec
Mage0Foxy <404729@vistacollege.nl>
2023-11-02 10:48:09 +0100

    Update Menu's 0.1.2
    Hebben nu een How To Play met Panel en Multiple Pages

--- MainMenu.gd
+++ MainMenu.gd
@@ -19,9 +19,29 @@ func _on_quit_pressed():
 func _on_options_pressed():
 	show_and_hide($Options, $Menu)
 
+func _on_how_to_play_pressed():
+	show_and_hide($"How To Play", $Menu)
+
 func show_and_hide(first, second):
 	first.show()
 	second.hide()
 
 func _on_button_pressed():
 	show_and_hide($Menu, $Options)
+
+func _on_back_pressed():
+	show_and_hide($Menu, $"How To Play")
+
+func _on_forward_pressed():
+	show_and_hide($"How To Play/VBoxContainer/2", $"How To Play/VBoxContainer/1")
+func _on_forward_2_pressed():
+	show_and_hide($"How To Play/VBoxContainer/3", $"How To Play/VBoxContainer/2")
+func _on_forward_3_pressed():
+	show_and_hide($"How To Play/VBoxContainer/4", $"How To Play/VBoxContainer/3")
+
+func _on_backwards_pressed():
+	show_and_hide($"How To Play/VBoxContainer/1", $"How To Play/VBoxContainer/2")
+func _on_backwards_2_pressed():
+	show_and_hide($"How To Play/VBoxContainer/2", $"How To Play/VBoxContainer/3")
+func _on_backwards_3_pressed():
+	show_and_hide($"How To Play/VBoxContainer/3", $"How To Play/VBoxContainer/4")
