a56676e69386b20fe6df0e038406579e03b54059
SCP-939-83 <512707@vistacollege.nl>
2023-11-07 11:15:49 +0100

    random solution generated

--- control.gd
+++ control.gd
@@ -5,6 +5,7 @@ func _ready():
 	generate_operator2()
 	generate_operator3()
 	generate_operator4()
+	generate_math()
 func generate_operator1():
 	var operators = ['+', '-', '*', '/']
 	var random_operator = operators[randi() % operators.size()]
@@ -21,3 +22,21 @@ func generate_operator4():
 	var operators = ['+', '-', '*', '/']
 	var random_operator = operators[randi() % operators.size()]
 	$VBoxContainer/HBoxContainer4/Button13.text = random_operator
+func generate_math():
+	var operators = ['+', '-', '*', '/']
+	var random_operator = operators[randi() % operators.size()]
+	var number1 = randi() % 20 + 1
+	var number2 = randi() % 20 + 1
+	var result = 0
+	match random_operator:
+		'+':
+			result = number1 + number2
+		'-':
+			result = number1 - number2
+		'*':
+			result = number1 * number2
+		'/':
+			result = number1 / number2
+
+	var text = str(result)
+	$VBoxContainer/Button.text = text
